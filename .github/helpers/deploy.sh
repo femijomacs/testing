705f40ea95356e5d13aace78f15c289866dfa5a7
femmyreuben <105896556+femmyreuben@users.noreply.github.com>
2023-05-31 12:01:42 -0600

    Update deploy.sh

--- .github/helpers/deploy.sh
+++ .github/helpers/deploy.sh
@@ -1,4 +1,4 @@
-# bash script to process the expressjs-api-dc-template and deploy it to a given namespace
+# bash script to process the expressjs-api-dc-template and deploy it to a dev namespace
 
 #!/bin/bash
 
@@ -7,8 +7,8 @@ objects=( $DC_TEMPLATE $SERVICE_TEMPLATE $ROUTE_TEMPLATE )
 for i in "${objects[@]}"
 do
     oc process -f /home/runner/work/femi-olaniran-IS27-devops-specialist-competition-req99120/femi-olaniran-IS27-devops-specialist-competition-req99120/openshift/templates/$DIR/"$i" --namespace=dev \
-        -p APPLICATION_NAME=expressjs-api \
-        -p IMAGESTREAM=expressjs-api \
-        -p ENVIRONMENT=dev | \
+        -p APPLICATION_NAME=$APPLICATION_NAME \
+        -p IMAGESTREAM=$IMAGESTREAM \
+        -p ENVIRONMENT=$ENVIRONMENT | \
         oc apply -f -
 done

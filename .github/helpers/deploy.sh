b772ec4baef5723721aa7346bf81d26af39c1411
femijomacs <128555347+femijomacs@users.noreply.github.com>
2023-06-12 00:20:33 -0600

    Update deploy.sh

--- .github/helpers/deploy.sh
+++ .github/helpers/deploy.sh
@@ -6,7 +6,7 @@ objects=( $DC_TEMPLATE $SERVICE_TEMPLATE $ROUTE_TEMPLATE )
 
 for i in "${objects[@]}"
 do
-    oc process -f /home/runner/work/femi-olaniran-IS27-devops-specialist-competition-req99120/femi-olaniran-IS27-devops-specialist-competition-req99120/openshift/templates/$DIR/"$i" --namespace=dev \
+    oc process -f /home/runner/work/testing/testing/openshift/templates/$DIR/"$i" --namespace=femmyreuben-dev \
         -p APPLICATION_NAME=$APPLICATION_NAME \
         -p IMAGESTREAM=$IMAGESTREAM \
         -p ENVIRONMENT=$ENVIRONMENT | \
